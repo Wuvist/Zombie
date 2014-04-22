abef29d702e4d7d11bb51b393aebee17468025a4
Wuvist <wuvist@gmail.com>
2014-04-22 17:09:15 +0800

    Add py

---
+++ zombie.py
@@ -0,0 +1,18 @@
+
+def get_time(C, F, X):
+	gap = X - C
+	gapF = gap * F
+	Xgap = X - gap
+
+	speed = 2.0
+	total = 0.0
+
+	while Xgap * speed < gapF:
+		total += C / speed
+		speed += F
+
+	total += X / speed
+
+	return total
+
+print get_time(37,7,54500000000)
